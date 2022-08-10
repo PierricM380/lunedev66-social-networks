12d3e6c93e617d9737b70641da823a945b728a58
PierricM380 <pierric.marye@wanadoo.fr>
2022-08-10 09:26:57 +0200

    Adding plugin into side and top bar wordpress's menu

--- includes/social-networks-functions.php
+++ includes/social-networks-functions.php
@@ -12,4 +12,27 @@ function my_admin_theme_style()
 add_action('admin_enqueue_scripts', 'my_admin_theme_style');
 add_action('login_enqueue_scripts', 'my_admin_theme_style');
 
-/*  */
+/* Creation of the sidebar */
+
+add_action("admin_menu", "social_media_sidebar");
+
+function social_media_sidebar()
+{
+    add_menu_page("My First Page", "Reseaux sociaux", "manage_options", "social-media", "social_media_page", "dashicons-share");
+}
+
+/* Creation of the topbar */
+
+add_action('admin_bar_menu', 'social_media_topbar', 999);
+
+// function who add associative array into topbar with a node
+function social_media_topbar($wp_admin_bar)
+{
+    $admin_topbar = array(
+        'id' => 'reseaux-sociaux',
+        'title' => 'Reseaux sociaux',
+        'href' => admin_url('admin.php?page=social-media') ,
+    );
+
+    $wp_admin_bar->add_node($admin_topbar);
+}
