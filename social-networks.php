3e4c2e09aa80024d46a53f016ff4d649cf02a320
PierricM380 <pierric.marye@wanadoo.fr>
2022-08-10 08:26:11 +0200

    Adding base files and dependency

--- social-networks.php
+++ social-networks.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
- * Plugin Name:       PierricM Social Networks
- * Plugin URI:        #
- * Description:       Partager vos réseaux sociaux
- * Version:           1.0
- * Author:            PierricM
- * Author URI:        #
- * Text Domain:       reseaux-sociaux
+ * Plugin Name: PierricM Social Networks
+ * Plugin URI: https://github.com/PierricM380/pierricm-social-networks
+ * Description: Décidez ou non de partager votre article sur un de vos réseaux sociaux préféré. Choisissez d'afficher les liens que vous souhaitez sur la page des articles.
+ * Author: PierricM
+ * Version: 1.0
+ * Author URI: https://pierricmaryedeveloppeurweb.netlify.app/
 */
 
+// added features file dependencies 
 require_once plugin_dir_path(__FILE__) . 'includes/social-networks-functions.php';
