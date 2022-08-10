710b22a3f501022af9e65699b6e21abb916070c8
PierricM380 <pierric.marye@wanadoo.fr>
2022-08-10 08:09:28 +0200

    initialization project file needed

---
+++ social-networks.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Plugin Name:       PierricM Social Networks
+ * Plugin URI:        #
+ * Description:       Partager vos réseaux sociaux
+ * Version:           1.0
+ * Author:            PierricM
+ * Author URI:        #
+ * Text Domain:       reseaux-sociaux
+*/
+
+require_once plugin_dir_path(__FILE__) . 'includes/social-networks-functions.php';
