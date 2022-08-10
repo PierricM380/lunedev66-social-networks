30f7af500890973f52e2f0f64a1bf5b9371ff599
PierricM380 <pierric.marye@wanadoo.fr>
2022-08-10 08:57:15 +0200

    Spelling grammar

--- social-networks.php
+++ social-networks.php
@@ -3,7 +3,7 @@
 /*
  * Plugin Name: PierricM Social Networks
  * Plugin URI: https://github.com/PierricM380/pierricm-social-networks
- * Description: Décidez ou non de partager votre article sur un de vos réseaux sociaux préféré. Choisissez d'afficher les liens que vous souhaitez sur la page des articles.
+ * Description: Décidez ou non de partager votre article sur un de vos réseaux sociaux préférés. Choisissez d'afficher les liens que vous souhaitez sur la page des articles.
  * Author: PierricM
  * Version: 1.0
  * Author URI: https://pierricmaryedeveloppeurweb.netlify.app/
